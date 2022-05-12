2fd85a2ff256853fea088ed56704541425947319
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-12 04:46:39 -0700

    Basic Core programming

---
+++ basicCoreProg/QuotientAndRemainder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCoreProg
+{
+    public class QuotientAndRemainder
+    {
+        public void quotientRemainder()
+        {
+            Console.Write("Enter the dividend number to calculate the Quotiend and Remainder : ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the divisor : ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            //Quotient logic
+            int quotient = dividend / divisor;
+
+            //Remainder logic
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is : {0}", quotient);
+            Console.WriteLine("Remainder is : {0}", remainder);
+        }
+    }
+}
---
+++ basicCoreProg/leapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCoreProg
+{
+    public class leapYear
+    {
+        public void year()
+        {
+            Console.Write("Enter 4 digit year to check leap year or not : ");
+            uint year = Convert.ToUInt32(Console.ReadLine());
+
+            //Checking if the entered value is 4 digit or not
+            if (year > 999 && year <= 9999)
+            {
+                //Leap Year condition
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                {
+                    Console.WriteLine("Year is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine("Year is not Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered year is not 4 digit");
+            }
+
+        }
+    }
+}
